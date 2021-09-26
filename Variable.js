c2cd0c32c46d6da41e7d9c1e0e2d4c288ab25375
csy1597 <qlseo3@naver.com>
2021-09-26 22:02:24 +0900

    variable

--- Variable.js
+++ Variable.js
@@ -1,6 +1,24 @@
 // 변수 저장용 및 연습용 
 
-console.log('hello world');
-var a = 5; 
-let b = '4';
-const c = 'chacha';
+// 1. 변수
+// var, let, const 
+
+let userInfo = 'chacha';
+console.log(userInfo);      // chacha 출력
+
+userInfo = 'micha';
+console.log(userInfo);      // micha 출력 let 재할당 가능 
+
+// let userInfo = 'code';
+// console.log(userInfo);      에러발생 let 재선언 불가능
+
+
+const name = 'cha';
+console.log(name);          // cha 출력
+
+// const name = 'cha2';
+// console.log(name);           에러발생 const 재선언 불가능
+// name = 'cha1';
+// console.log(name);           에러발생 const 재할당 불가능
+
+
