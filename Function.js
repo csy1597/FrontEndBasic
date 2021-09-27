f2c67992de9946fda6b4fe4da0f1bb0e75416398
csy1597 <qlseo3@naver.com>
2021-09-27 23:40:50 +0900

    function4

--- Function.js
+++ Function.js
@@ -27,5 +27,20 @@ console.log(dom);
 // 3. default parameter 
 function showMsg(msg, from){
     console.log(`${msg} by ${from}`);
+
 }
-showMsg('hi');
+showMsg('hi');  // hi by undefined 출력
+
+//4. Rest parameter 
+// '... 이것은 배열형태로 전달된다'라는 의미한다. 
+function printAll(...arg){
+    for(const i =1;i<arg.length;i++){
+        console.log(arg[i]);
+    }
+    /* 
+    for(arg of args){   args -> 배열의 모든 데이터
+        console.log(arg);
+    }
+    */
+}
+printAll('안녕', 'hello', 'hi');
