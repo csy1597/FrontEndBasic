9afc9fa6a99fe637a1db7e03956280b1d5b0e549
csy1597 <qlseo3@naver.com>
2021-10-04 22:26:10 +0900

    map

--- arrTest.js
+++ arrTest.js
@@ -28,3 +28,17 @@ console.log(newCar.indexOf('쉐보레'));
 console.log(newCar.indexOf('기아'));
 
 console.log(newCar.includes('현대'));
+
+console.log('-----------------------------')
+const fruits = [
+    {name:'apple', size:'small', weight:2}
+    , {name:'banana', size:'medium', weight:2}
+    , {name:'wetermelon', size:'big', weight:10}
+    , {name:'orange', size:'small', weight:1}
+];
+
+const map = fruits.map(function(fruit){
+    return `나 ${fruit.name} 먹고싶어!!!`
+});
+console.log(map);
+console.log('-----------------------------')
