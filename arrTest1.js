7caf4ab9f2e58e28ea9f354608c8b23c5be08cb6
csy1597 <qlseo3@naver.com>
2021-10-05 23:28:31 +0900

    concat

--- arrTest1.js
+++ arrTest1.js
@@ -1,9 +1,14 @@
 // concat, map, filter
 const grades = [
     {name: 'math', score: 90, grade:'A'}
-    , {name: 'english', score: 90, grade:'A'}
-    , {name: 'korean', score: 90, grade:'A'}
-    , {name: 'science', score: 90, grade:'A'}
-    , {name: 'social', score: 90, grade:'A'}
+    , {name: 'english', score: 96, grade:'A'}
+    , {name: 'korean', score: 85, grade:'B'}
+    , {name: 'science', score: 70, grade:'C'}
+    , {name: 'social', score: 65, grade:'D'}
 ];
+
+const concat = [{name:'history', score: 88, grade:'B'}];
+
+const newGrade = grades.concat(concat);
+console.log(newGrade);
 
