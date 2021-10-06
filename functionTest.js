3b7984f4d78e47ea4c98fbeea06735875375ace3
csy1597 <qlseo3@naver.com>
2021-10-07 01:22:03 +0900

    function3

--- functionTest.js
+++ functionTest.js
@@ -1,4 +1,5 @@
 //  function
+
 //  사칙연산
 function add(num1,num2){
     return num1 + num2
@@ -27,3 +28,22 @@ function division(num1, num2){
 const result4 = division(5,2);
 console.log(result4);
 console.log('--------------------------')
+
+// callback 
+
+function stuGrade(score, pass, fail){
+    if(score >= 60){
+        pass();
+    } else{
+        fail();
+    }
+}
+
+function pass(){
+    console.log('pass');
+}
+function fail(){
+    console.log('fail');
+}
+
+stuGrade(60, pass, fail);
