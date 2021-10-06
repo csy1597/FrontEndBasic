369820443da88e33ff937b61e68bb8f3534c4cdc
csy1597 <qlseo3@naver.com>
2021-10-07 00:38:52 +0900

    +,-,*,/

---
+++ functionTest.js
@@ -0,0 +1,29 @@
+//  function
+//  사칙연산
+function add(num1,num2){
+    return num1 + num2
+};
+const result1 = add(5,4);
+console.log(result1);
+console.log('--------------------------')
+
+function subtract(num1,num2){
+    return num1 - num2
+}
+const result2 = subtract(5,4);
+console.log(result2);
+console.log('--------------------------')
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+const result3 = multiply(5,2);
+console.log(result3);
+console.log('--------------------------')
+
+function division(num1, num2){
+    return num1 / num2;
+}
+const result4 = division(5,2);
+console.log(result4);
+console.log('--------------------------')
